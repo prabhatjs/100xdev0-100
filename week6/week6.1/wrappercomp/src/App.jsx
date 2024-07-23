600670ca0a8852e8ba2490d536a868f1c558a4ba
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-07-23 23:02:43 +0530

    add week 6 2nd commit

--- week6/week6.1/wrappercomp/src/App.jsx
+++ week6/week6.1/wrappercomp/src/App.jsx
@@ -1,30 +1,44 @@
 import { useState } from 'react'
 function App() {
+  // return<div>
+  //   <CardWrapper headingcomp={<CardHeaderComp/>} aboutcomp={<CardDeatilsComp/>}></CardWrapper>
+  // </div>
+
   return<div>
-    <CardWrapper headingcomp={<CardHeaderComp/>} aboutcomp={<CardDeatilsComp/>}></CardWrapper>
+    <CardWrapper >
+      <h1>Shoes</h1>
+    </CardWrapper>
+    <CardWrapper>
+    <p>This shoes is nice!</p>
+    </CardWrapper>
   </div>
+
 }
 
-function CardHeaderComp(){
-  return <div>
-    <h1>shoes</h1>
-  </div>
-}
+// function CardHeaderComp(){
+//   return <div>
+//     <h1>shoes</h1>
+//   </div>
+// }
 
-function CardDeatilsComp(){
-  return<div>
-    <p>This shoes is so nice!</p>
-  </div>
-}
+// function CardDeatilsComp(){
+//   return<div>
+//     <p>This shoes is so nice!</p>
+//   </div>
+// }
 
-function CardWrapper({headingcomp,aboutcomp}){
-  return <>
-  <div style={{border:"2px solid red"}}>
-      {headingcomp}
-  </div>
-  <div style={{border:"2px solid blue"}}>
-      {aboutcomp}
-  </div>
-  </>
+function CardWrapper({children}){
+return <div style={{border:"2px solid red"}}>
+  {children}
+</div>
+
+  // return <>
+  // <div>
+  //     {headingcomp}
+  // </div>
+  // <div style={{border:"2px solid blue"}}>
+  //     {aboutcomp}
+  // </div>
+  // </>
 }
 export default App
